823cdec509b2972ca43b457ec8d32dc7c769e835
max <eroken@mail.ru>
2024-03-12 00:44:51 +0300

    commit 2

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/MaksimErs/gol/gret"
+)
 
 func main() {
 	var name string
 	fmt.Println("Введите имя:")
 	fmt.Scan(&name)
 	fmt.Println("Hello", name)
+	gret.Hell()
 }
